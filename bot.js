91f35850efac4b3d56bd6eac2fe3f6705fb985ef
Woolleyx12 <Woolleyx12@hotmail.co.uk>
2016-02-11 23:00:23 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,7 +18,7 @@ function respond() {
   } 
   else if(request.text && botRegexDL.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("http://daddyleagues.com/mlyp/team/"+request.text.substring(5,8)+"/depthchart");
+    postMessage("http://www.pornhub.com");
     this.res.end();
   } 
   else if(request.text && botRegexSalt.test(request.text)) {
@@ -43,14 +43,14 @@ function respond() {
   } 
   else if(request.text && botRegexSC.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("http://daddyleagues.com/mlyp/team/"+request.text.substring(5,8)+"/schedule");
+    postMessage("http://www.pornhub.com");
     this.res.end();
   }
   else if(request.text && botRegexP.test(request.text)) {
     this.res.writeHead(200);
     var req = request.text.substring(5,request.text.length);
     var rep = req.replace(/ /,"+");
-    postMessage("http://daddyleagues.com/mlyp/players?name="+rep+"&position=all&team=all");
+    postMessage("http://www.pornhub.com");
     this.res.end();
   }  
 
